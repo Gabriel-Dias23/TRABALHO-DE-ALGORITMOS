cc886f05ba9305ab4a0de84b3756bbc98be93753
Gabriel Dias <bieldias074@gmail.com>
2025-04-15 23:24:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel.Design;
+namespace FilaVazia
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Queue<int> Filavazia = new Queue<int>();
+
+            if (Filavazia.Count == 0)
+            {
+                Console.WriteLine("A fia está vazia");
+
+            }
+
+            Filavazia.Enqueue(1);
+            Filavazia.Enqueue(2);
+            Filavazia.Enqueue(3);
+
+            if (Filavazia.Count > 0)
+            {
+                Console.WriteLine("A fila está cheia");
+            }
+
+
+            foreach (int fila in Filavazia)
+            {
+                Console.WriteLine(fila);
+            }
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
